87a62ea5af13e1ec91cfb94d55cd6965ce3b2cf9
priyanka <priyankarr140@gmail.com>
2025-10-28 11:51:43 +0530

    jh

--- src/test/java/jenkinspractice/coforge_jenkins_nov_2025/LoginTest.java
+++ src/test/java/jenkinspractice/coforge_jenkins_nov_2025/LoginTest.java
@@ -5,7 +5,7 @@ public class LoginTest {
 	@Test
 	void login()
 	{
-		System.out.println("payment6 TEST FUNCTIONALITY***!!!AABCDEE");
+		System.out.println("payment6 TEST FUNCTIONALITY***!!!AABCDEEF");
 	}
 
 }
