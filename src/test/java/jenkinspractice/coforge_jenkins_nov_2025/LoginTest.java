e1f52ded81c5443d0e99cb5c526bb370e58f72ea
priyanka <priyankarr140@gmail.com>
2025-10-28 10:47:53 +0530

    initial commit

---
+++ src/test/java/jenkinspractice/coforge_jenkins_nov_2025/LoginTest.java
@@ -0,0 +1,11 @@
+package jenkinspractice.coforge_jenkins_nov_2025;
+import org.testng.annotations.*;
+public class LoginTest {
+	
+	@Test
+	void login()
+	{
+		System.out.println("payment6 TEST FUNCTIONALITY");
+	}
+
+}
